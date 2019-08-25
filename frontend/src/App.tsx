58c7946728fa864679f1d08d8ca5a4a884df917b
circlezero <jwy0565@gmail.com>
2019-08-25 16:58:26 +0900

    Adjust React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { Layout } from "antd";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Home, Login, Signup } from "./pages";
 import Header from "./layout/Header";
+
 import "App.scss";
 
 const App: React.FC = () => {
   const { Footer, Content } = Layout;
   return (
     <div className="App">
-      <Layout>
-        <Header>Header</Header>
-        <Content>Content</Content>
-        <Footer>Footer</Footer>
-      </Layout>
+      <Router>
+        <Layout>
+          <Header>Header</Header>
+          <Content>Content</Content>
+          <Footer>Footer</Footer>
+        </Layout>
+
+        <Route path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+      </Router>
     </div>
   );
 };
